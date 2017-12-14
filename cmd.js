58f4d00407de5af380961818ce2a14c5e1b906a7
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2017-12-14 13:47:57 +0100

    Add other files

---
+++ cmd.js
@@ -0,0 +1,149 @@
+#!/usr/bin/env node
+
+const fs                                = require('fs');
+const {YMAP, YTYP, Vector3, Quaternion} = require('./ymap.js');
+const program                           = require('commander');
+const byline                            = require('byline');
+
+const pkg = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8'));
+
+const getDistanceBetweenCoords = function(coords1, coords2){
+	return Math.sqrt(
+		(coords2.x - coords1.x) * (coords2.x - coords1.x) +
+		(coords2.y - coords1.y) * (coords2.y - coords1.y) +
+		(coords2.z - coords1.z) * (coords2.z - coords1.z)
+	)
+}
+
+// Program
+function parsePosition(val) {
+	
+	const buff = val.split(',').map(e => parseFloat(e));
+
+	if(
+		typeof buff[0] != 'undefined' &&
+		typeof buff[1] != 'undefined' &&
+		typeof buff[2] != 'undefined'
+	) {
+		return new Vector3(buff[0], buff[1], buff[2]);
+	}
+}
+
+function parseRotation(val) {
+
+	const buff = val.split(',').map(e => parseFloat(e));
+
+	if(
+		typeof buff[0] != 'undefined' &&
+		typeof buff[1] != 'undefined' &&
+		typeof buff[2] != 'undefined' &&
+		typeof buff[3] != 'undefined'
+	) {
+		return new Quaternion(buff[0], buff[1], buff[2], buff[3]);
+	}
+
+}
+
+program
+	.version(pkg.version)
+  .description('YMAP/YTYP toolkit')
+  .option('-i,   --inject2ytp',        'Inject YMAP to YTYP')
+  .option('-f,   --find',              'Find YMAP')
+  .option('-yt,  --ytyp     [file]',   'YTYP file')
+  .option('-ym,  --ymap     [file]',   'YMAP file')
+  .option('-r,   --room     [name]',   'Room name')
+  .option('-pos, --position <pos>',    'Initial position => x,y,z',   parsePosition)
+  .option('-rot, --rotation <rot>',    'Initial rotation => x,y,z,w', parseRotation)
+  .option('-n,   --name     [name]',   'Output file name without extension')
+  .option('-r,   --radius   [radius]', 'Radius')
+;
+
+program.on('--help', function() {
+	
+	console.log('');
+	console.log('  Examples:');
+	console.log('');
+	console.log('    ymap --find --position 1009.54500000,-3196.59700000,-39.99353000 --radius 25');
+	console.log('    ymap --inject2ytp --ytyp bkr_biker_dlc_int_ware01.ytyp.xml --ymap weed2.ymap.xml --room MethMain --position 1009.54500000,-3196.59700000,-39.99353000 --rotation 0.0,0.0,0.0,1.0 --name merged');
+	console.log('');
+
+})
+
+program.parse(process.argv)
+
+if(program.inject2ytp) {
+
+	if(
+		typeof program.ytyp     != 'undefined' &&
+		typeof program.ymap     != 'undefined' &&
+		typeof program.position != 'undefined' &&
+		typeof program.rotation != 'undefined' &&
+		typeof program.name     != 'undefined'
+	) {
+
+		if(typeof program.room == 'undefined')
+			program.room = null;
+
+		if(!fs.existsSync(program.ytyp)) {
+			console.error('File ' + program.ytyp + ' not found');
+			return;
+		}
+
+		if(!fs.existsSync(program.ymap)) {
+			console.error('File ' + program.ymap + ' not found');
+			return;
+		}
+
+		const ytyp = new YTYP(fs.readFileSync(program.ytyp, 'utf8'));
+		const ymap = new YMAP(fs.readFileSync(program.ymap, 'utf8'));
+
+		ytyp.on('entity.merge', function(i, max) {
+			console.error('Merge => ' + i + '/' + max)
+		})
+
+		ytyp.on('entity.normalize', function(i, max) {
+			console.error('Normalize => ' + i + '/' + max)
+		})
+
+		ytyp.merge(ymap, program.room, program.position, program.rotation);
+		
+		ytyp.normalizeHashes();
+		
+		fs.writeFileSync(program.name + '.ytyp.xml', ytyp.build());
+
+	}
+
+} else if(program.find) {
+
+	if(typeof program.position == 'undefined') {
+		console.error('--position required');
+		return;
+	}
+
+	if(typeof program.radius == 'undefined') {
+		program.radius = 25;
+	}
+
+	const stream = byline(fs.createReadStream(__dirname + '/data/ymap_coords.txt', {encoding: 'utf8'}));
+
+	stream.on('data', function(line) {
+
+		const buff  = line.split('|')
+		const buff2 = buff[0] .split(',')
+		
+		if(typeof buff[1] != 'undefined'){
+
+			const buff3    = buff[1] .split(',')
+			const name     = buff2[0];
+			const entity   = buff2[1];
+			const coords   = {x: parseFloat(buff3[0]), y: parseFloat(buff3[1]), z: parseFloat(buff3[2])};
+			const distance = getDistanceBetweenCoords(program.position, coords);
+
+			if(distance <= program.radius)
+				console.log(name);
+
+		}
+
+	});
+
+}
---
+++ ymap.js
@@ -0,0 +1,263 @@
+const EventEmitter  = require('events');
+const DOMParser     = require('xmldom').DOMParser;
+const XMLSerializer = require('xmldom').XMLSerializer;
+const xpath         = require('xpath').useNamespaces();
+const THREE         = require('three');
+
+const serializer = new XMLSerializer();
+
+// https://stackoverflow.com/questions/42812861/three-js-pivot-point
+function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
+	
+	var pointIsWorld = (pointIsWorld === undefined) ? false : pointIsWorld;
+  
+	if(pointIsWorld)
+		obj.parent.localToWorld(obj.position); // compensate for world coordinate
+  
+	obj.position.sub(point); // remove the offset
+	obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
+	obj.position.add(point); // re-add the offset
+  
+	if(pointIsWorld)
+		obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
+  
+	obj.rotateOnAxis(axis, theta); // rotate the OBJECT
+
+}
+
+class YMAP extends EventEmitter {
+
+	get entities() {
+		return xpath('//CMapData/entities/Item', this.doc);
+	}
+
+	constructor(xmlString) {
+		super();
+		this.parse(xmlString);
+	}
+
+	parse(xmlString) {
+		this.doc = new DOMParser().parseFromString(xmlString);
+	}
+
+	build() {
+		return serializer.serializeToString(this.doc);
+	}
+
+	merge(target) {
+
+		if(target instanceof YMAP) {
+
+			const entities = target.entities;
+
+			for(let i=0; i<entities.length; i++) {
+				this.emit('entity.merge', i, entities.length - 1)
+				const newNode = this.addEntity(entities[i]);
+			}
+
+		} else if(target instanceof YTYP) {
+			// TODO
+		}
+
+	}
+
+	addEntity(entity) {
+		
+		const entities = xpath('//CMapData/entities', this.doc)[0];
+		const newNode  = entity.cloneNode(true);
+
+		entities.appendChild(newNode);
+	}
+
+	normalizeHashes() {
+
+		const entities       = this.entities;
+		const archetypeNames = xpath('//CMapData/entities/Item/archetypeName', this.doc);
+
+		for(let i=0; i<archetypeNames.length; i++) {
+			
+			this.emit('entity.normalize', i, archetypeNames.length - 1)
+			
+			if(archetypeNames[i].textContent.substr(0, 2) == '0x')
+				archetypeNames[i].textContent = 'hash_' + archetypeNames[i].textContent.substr(2, archetypeNames[i].textContent.length).toUpperCase();
+
+		}
+
+	}
+
+}
+
+class YTYP extends EventEmitter {
+
+	get entities() {
+		return xpath('//CMapTypes/archetypes/Item[@type="CMloArchetypeDef"]/entities/Item', this.doc);
+	}
+
+	constructor(xmlString) {
+		super();
+		this.parse(xmlString);
+	}
+
+	parse(xmlString) {
+		
+		this.doc                 = new DOMParser().parseFromString(xmlString);
+		this.entitiesContainer   = xpath('//CMapTypes/archetypes/Item[@type="CMloArchetypeDef"]/entities', this.doc)[0];
+		this.roomAttachedObjects = {};
+		const rooms              = xpath('//CMapTypes/archetypes/Item[@type="CMloArchetypeDef"]/rooms/Item', this.doc);
+
+		for(let i=0; i<rooms.length; i++) {
+			const roomName                     = rooms[i].getElementsByTagName('name')[0].textContent;
+			const itemIds                      = rooms[i].getElementsByTagName('attachedObjects')[0].textContent.split(/[\r\n\t\s]{1,}/);
+			this.roomAttachedObjects[roomName] = itemIds.filter(e => e != '').map(e => parseInt(e));
+		}
+
+	}
+
+	build() {
+
+		for(let roomName in this.roomAttachedObjects) {
+
+			const xpathExpression     = '//CMapTypes/archetypes/Item[@type="CMloArchetypeDef"]/rooms/Item/name[text()="' + roomName + '"]/parent::Item/attachedObjects/text()';
+			const roomAttachedObjects = xpath(xpathExpression, this.doc)[0];
+			const depth               = xpath(xpathExpression + '/ancestor::*', this.doc).length;
+			let spaces                = '';
+			let lastSpaces            = '';
+			const entities            = this.entities;
+
+			for(let i=0; i<depth; i++) {
+				
+				spaces = spaces + '  ';
+				
+				if(i < depth - 1)
+					lastSpaces = lastSpaces + '  ';
+			}
+
+			roomAttachedObjects.textContent = "\r\n" + spaces + this.roomAttachedObjects[roomName].join("\r\n" + spaces) + "\r\n" + lastSpaces;
+
+		}
+
+		return serializer.serializeToString(this.doc);
+	}
+
+	merge(target, roomName, initialPos, initialRot) {
+
+		initialPos = initialPos || new THREE.Vector3(0.0, 0.0, 0.0);
+		initialRot = initialRot || new THREE.Quaternion(0.0, 0.0, 0.0, 1.0)
+
+		if(target instanceof YMAP) {
+
+			const entities = target.entities;
+
+			for(let i=0; i<entities.length; i++) {
+				
+				this.emit('entity.merge', i, entities.length - 1)
+
+				const newNode = this.addEntity(entities[i], initialPos, initialRot);
+
+				if(roomName) {
+
+					this.addEntityToRoom(newNode, roomName);
+
+				} else {
+
+					for(let roomName in this.roomAttachedObjects)
+						this.addEntityToRoom(newNode, roomName);
+				}
+			}
+
+		} else if(target instanceof YTYP) {
+			// TODO
+		}
+
+	}
+
+	addEntity(entity, initialPos, initialRot) {
+		
+		const newNode       = entity.cloneNode(true);
+		const positionNode  = newNode.getElementsByTagName('position')[0];
+		const rotationNode  = newNode.getElementsByTagName('rotation')[0];
+		const object3D      = new THREE.Object3D();;
+
+		// Position
+		object3D.position.set(
+			parseFloat(positionNode.getAttribute('x')),
+			parseFloat(positionNode.getAttribute('y')),
+			parseFloat(positionNode.getAttribute('z')),
+		)
+
+		const newPos = object3D.position.sub(initialPos)
+
+		// Rotation
+		object3D.applyQuaternion(new THREE.Quaternion(
+			parseFloat(rotationNode.getAttribute('x')),
+			parseFloat(rotationNode.getAttribute('y')),
+			parseFloat(rotationNode.getAttribute('z')),
+			parseFloat(rotationNode.getAttribute('w')),
+		));
+
+		const rotationEuler = new THREE.Euler()
+		rotationEuler.setFromQuaternion(initialRot);
+
+		rotateAboutPoint(object3D, initialPos, new THREE.Vector3(1.0, 0.0, 0.0), rotationEuler.x * Math.PI / 180);
+		rotateAboutPoint(object3D, initialPos, new THREE.Vector3(0.0, 1.0, 0.0), rotationEuler.y * Math.PI / 180);
+		rotateAboutPoint(object3D, initialPos, new THREE.Vector3(0.0, 0.0, 1.0), rotationEuler.z * Math.PI / 180);
+
+		const newRot = object3D.quaternion;
+
+		// Set new values
+		positionNode.setAttribute('x', newPos.x.toFixed(8))
+		positionNode.setAttribute('y', newPos.y.toFixed(8))
+		positionNode.setAttribute('z', newPos.z.toFixed(8))
+
+		rotationNode.setAttribute('x', newRot.x.toFixed(8))
+		rotationNode.setAttribute('y', newRot.y.toFixed(8))
+		rotationNode.setAttribute('z', newRot.z.toFixed(8))
+		rotationNode.setAttribute('w', newRot.w.toFixed(8))
+
+		this.entitiesContainer.appendChild(newNode);
+
+		return newNode;
+	}
+
+	addEntityToRoom(entity, roomName) {
+
+		if(typeof this.roomAttachedObjects[roomName] != 'undefined') {
+
+			const entities = this.entities;
+
+			for(let i=0; i<entities.length; i++) {
+				if(entity == entities[i]) {
+					this.roomAttachedObjects[roomName].push(i);
+					break;
+				}
+			}
+
+		}
+
+	}
+
+	normalizeHashes() {
+
+		const entities       = this.entities;
+		const archetypeNames = xpath('//CMapTypes/archetypes/Item[@type="CMloArchetypeDef"]/entities/Item/archetypeName', this.doc);
+
+		for(let i=0; i<archetypeNames.length; i++) {
+			
+			this.emit('entity.normalize', i, archetypeNames.length - 1)
+			
+			if(archetypeNames[i].textContent.substr(0, 2) == '0x')
+				archetypeNames[i].textContent = 'hash_' + archetypeNames[i].textContent.substr(2, archetypeNames[i].textContent.length).toUpperCase();
+
+		}
+
+	}
+
+}
+
+
+module.exports = {
+	YMAP      : YMAP,
+	YTYP      : YTYP,
+	Vector3   : THREE.Vector3,
+	Quaternion: THREE.Quaternion,
+};
